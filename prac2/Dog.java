25b3012abea4184eb29e5108c3e7c643fec5fc58
Ruhul Amin <mail.ruhul005@gmail.com>
2024-09-05 22:58:44 +0930

    Update Dog.java

--- prac2/Dog.java
+++ prac2/Dog.java
@@ -3,7 +3,19 @@ import java.util.Set;
 
 public class Dog {
 
+    private String name ="unknown";
+    private String dogBreed = "unknown";
+    private int regNum =-1;
 
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "name='" + name + '\'' +
+                ", dogBreed='" + dogBreed + '\'' +
+                ", regNum=" + regNum +
+                '}';
+    }
 }
 
 
